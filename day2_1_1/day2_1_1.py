53bbd8a017f22590d2bac5ee884b64eb9680b94a
winine <rmleeno1@gmail.com>
2019-06-13 07:21:40 +0900

    tensorflow.keras import preprocessing

--- day2_1_1/day2_1_1.py
+++ day2_1_1/day2_1_1.py
@@ -21,3 +21,23 @@ print("word_list : %s" %word_list)
 
 char_to_int = dict((c, i) for i, c in enumerate(word_list))
 char_to_int
+
+integer_encoded = []
+for token_sentence in token_sentences:
+  integer_encoded.append([char_to_int[char] for char in token_sentence])  
+  
+print(integer_encoded)
+
+from tensorflow.keras import preprocessing
+# 전시간에 배운 preprocessing의 Tokenizer 함수를 이용하여 Instance를 만든다.
+# fit_on_texts 함수에 Token하고자 하는 copus 를 넣는다.
+# 문장을 index 처리 한다.
+
+tokenizer = preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts(samples) 
+sequence = tokenizer.texts_to_sequences(samples) 
+
+word_index = tokenizer.word_index
+
+print("token_sentences : %s " %sequence)
+print("word_list : %s" %word_index)
